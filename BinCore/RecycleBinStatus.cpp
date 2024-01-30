89a323e7d9665975f79a36a4c2f50a96d9898ace
Awesome <awesome_devnet@outlook.com>
2024-01-30 17:37:00 +0900

    Add project files.

---
+++ BinCore/RecycleBinStatus.cpp
@@ -0,0 +1,18 @@
+#include <Windows.h>
+#include <shellapi.h>
+
+extern "C" __declspec(dllexport) bool IsRecycleBinEmpty() {
+	SHQUERYRBINFO recycleBinInfo;
+	recycleBinInfo.cbSize = sizeof(SHQUERYRBINFO);
+
+	// Get Information of Recycle Bin
+	HRESULT hr = SHQueryRecycleBinW(NULL, &recycleBinInfo);
+	if (SUCCEEDED(hr)) {
+		// Check the number of items in the Recycle Bin
+		// If Empty : true / else : false
+		return (recycleBinInfo.i64NumItems == 0);
+	}
+
+	// error :: return false
+	return false;
+}
